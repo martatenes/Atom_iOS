d04ffc871b1dc84402757aa63c02b11fc1415543
Marta <marta@MacBook-Pro-de-Marta-2.local>
2020-07-08 23:34:30 +0200

    Creación de proyecto y vista Splash

---
+++ Atom/Splash/SplashVC.swift
@@ -0,0 +1,20 @@
+//
+//  SplashVC.swift
+//  Atom
+//
+//  Created by Marta on 8/7/20.
+//  Copyright © 2020 Marta. All rights reserved.
+//
+
+import UIKit
+
+
+class SplashVC: UIViewController{
+    
+    @IBOutlet weak var logoView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
