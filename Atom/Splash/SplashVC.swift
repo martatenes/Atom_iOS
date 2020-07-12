7536745cbb676c306868caffbe9ab5e6285b4dce
Marta <marta@MacBook-Pro-de-Marta-2.local>
2020-07-12 17:08:35 +0200

    Mensaje alert

--- Atom/Splash/SplashVC.swift
+++ Atom/Splash/SplashVC.swift
@@ -67,10 +67,8 @@ extension SplashVC: SplashView{
     }
     
     func showError(){
-        let alert = UIAlertController(title: "My Alert", message: "This is an alert.", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-            NSLog("The \"OK\" alert occured.")
-        }))
+        let alert = UIAlertController(title: "Error", message: "Ocurrió un error al obtener la información", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Aceptar", style: .default))
         self.present(alert, animated: true, completion: nil)
     }
 }
